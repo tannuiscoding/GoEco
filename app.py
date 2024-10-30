b52230c325e9d5cb284db68d1da4670f3fcf6b34
tannuiscoding <tannuchoudhary2000@gmail.com>
2024-10-30 14:42:53 +0530

    updated project

--- app.py
+++ app.py
@@ -4,25 +4,21 @@ import os
 
 app = Flask(__name__)
 
-API_KEY = 'your api key'  # Replace with your actual WeatherAPI key
+API_KEY = 'your api key' 
 
-# Function to get weather data from WeatherAPI.com
 def get_weather_data(city):
     url = f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no'
     response = requests.get(url)
     data = response.json()
     return data
 
-
-# Function to predict energy source
 def predict_energy_source(weather_data, sunshine_hours, is_near_water=False, is_geothermal_region=False):
-    wind_speed = weather_data['current']['wind_kph'] * 0.27778  # Convert kph to m/s
-    rainfall = weather_data['current'].get('precip_mm', 0)  # Get rainfall in mm
-    condition = weather_data['current']['condition']['text']  # Weather condition
+    wind_speed = weather_data['current']['wind_kph'] * 0.27778  
+    rainfall = weather_data['current'].get('precip_mm', 0)  
+    condition = weather_data['current']['condition']['text']  
 
     resources= []
 
-    # Example logic to predict energy source based on weather conditions and sunshine hours
     if sunshine_hours > 5:
         resources.append("Solar Energy")
     if is_near_water:
@@ -47,10 +43,8 @@ def predict():
     is_near_water = request.form.get('is_near_water') == 'on'
     is_geothermal_region = request.form.get('is_geothermal_region') == 'on'
 
-    # Get the weather data for the specified city
     weather_data = get_weather_data(city)
 
-    # Predict the energy source based on the weather and geographical data
     energy_source = predict_energy_source(weather_data, sunshine_hours, is_near_water, is_geothermal_region)
     
     return jsonify({'energy_source': energy_source})
--- static/README.md
+++
@@ -1,2 +0,0 @@
-# GoEco
-GoEco is a project that solves the issue of energy sources. This website is going to help people in finding which renewable energy source is best for their location.
